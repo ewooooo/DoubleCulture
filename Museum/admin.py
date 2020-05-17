7c80ff45f51c8c8b431f998dcc40d58b01811187
ewooooo <nuw577@naver.com>
2020-05-17 17:06:27 +0900

    관리자페이지 add , 스템프 add

--- Museum/admin.py
+++ Museum/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import  Museum,StudentProject,Watch
+from import_export.admin import ExportActionModelAdmin, ImportExportMixin, ImportMixin
+
+class MuseumAdmin(ImportExportMixin, admin.ModelAdmin):
+    pass
+
+class StudentProjectAdmin(ImportExportMixin, admin.ModelAdmin):
+    pass
+
+class WatchAdmin(ImportExportMixin, admin.ModelAdmin):
+    pass
+
+
+# Register your models here.
+admin.site.register(Museum,MuseumAdmin)
+admin.site.register(StudentProject,StudentProjectAdmin)
+admin.site.register(Watch,WatchAdmin)
