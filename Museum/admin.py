1034e6ea2b35e30558082eeede3077958c5fe73c
seon1346 <61758729+seon1346@users.noreply.github.com>
2020-05-21 00:51:35 +0900

    20200521

--- Museum/admin.py
+++ Museum/admin.py
@@ -1,21 +1,33 @@
 from django.contrib import admin
-
-# Register your models here.
-from django.contrib import admin
-from .models import  Museum,StudentProject,Watch
+from .models import StudentProject,Museum,Watch
 from import_export.admin import ExportActionModelAdmin, ImportExportMixin, ImportMixin
-
-class MuseumAdmin(ImportExportMixin, admin.ModelAdmin):
-    pass
+from rangefilter.filter import DateRangeFilter, DateTimeRangeFilter
 
 class StudentProjectAdmin(ImportExportMixin, admin.ModelAdmin):
+    list_display = ['user', 'CompleteStatue','created','modify_date']
+    search_fields = ['created', 'user__username']
+    list_filter = ('CompleteStatue',) 
     pass
 
-class WatchAdmin(ImportExportMixin, admin.ModelAdmin):
+class MuseumAdmin(ImportExportMixin, admin.ModelAdmin):
+    list_display = ['museum_number', 'howtogo', 'quiz1','quiz2', 'quiz3']
+
     pass
 
+class watchAdmin(ImportExportMixin, admin.ModelAdmin):
+    list_display = ['project','museum', 'quiz1_answer', 'quiz2_answer','quiz3_answer']
+    search_fields = ['project__user__username','museum__museum_number', 'create_date', 'modify_date']
+    list_filter =( ('create_date',DateRangeFilter),('modify_date',DateRangeFilter))# pip install django-admin-rangefilter, installed app ='rangefilter',
+    pass
+
+
+class number_watchAdmin(ImportExportMixin, admin.ModelAdmin):
+    pass
 
 # Register your models here.
-admin.site.register(Museum,MuseumAdmin)
 admin.site.register(StudentProject,StudentProjectAdmin)
-admin.site.register(Watch,WatchAdmin)
+admin.site.register(Museum,MuseumAdmin)
+admin.site.register(Watch,watchAdmin)
+
+
+
